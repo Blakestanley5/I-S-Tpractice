e476ab4f674fae7b696c1ff3bb4022ac77896068
BlakeStanley5 <blakestanley542@gmail.com>
2020-10-27 13:22:26 -0500

    practice

---
+++ I,S,T-practice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace I_S_T_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how old are you?");
+            
+            var age = Convert.ToInt32(Console.ReadLine());
+            
+            var answer = (age >= 21) ? $"Based on your age {age} you are permited to this site." : $"Based on your age {age} you are not permited on this site.";
+           
+            Console.WriteLine(answer);
+
+
+            
+
+                
+            
+
+        }
+    }
+}
